c905fc377af08b8eb5b2b356e6c74d72b6ad1e76
Elite6802 <95652067+Elite6802@users.noreply.github.com>
2021-12-15 17:51:34 +0300

    Add files via upload

---
+++ form_submission.php
@@ -0,0 +1,20 @@
+<?php
+$name = $_POST['name'];
+$visitor_mail = $_POST['mail'];
+$message = $_POST['comment'];
+
+$email_from = "kelvinmutinda218@gmail.com";
+
+$email_subject = "COMMENTS";
+
+$email_body = "User Name:"$name\n;
+               "User Email:"$visitor_mail\n;
+                "User Message:"$message\n;
+               
+ 
+ $to = "kelvinmutinda347@gmail.com";
+ $headers = "From:"$email_from\r\n;
+ $headers = "Reply to"$visitor_mail\r\n;
+ mail($to,$email_subject,$email_body,$headers);
+ header("Location:contact.html")
+?>
